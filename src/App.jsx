07bef9530b584b73faad7ef17b3be5dc0607308e
Yao Zu <1796381757@qq.com>
2025-05-05 22:25:46 +0800

    Hero 部分

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "./components/Hero.jsx";
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Hero />
+
+      <section />
+    </main>
+  );
+};
+
+export default App;
