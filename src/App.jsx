78886f991578d3593d9fd3129674ddc43b33c941
Yao Zu <1796381757@qq.com>
2025-05-05 23:29:04 +0800

    About 和 Animated Title部分

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Hero from "./components/Hero.jsx";
+import About from "./components/About.jsx";
 
 const App = () => {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <Hero />
 
-      <section />
+      <About />
     </main>
   );
 };
